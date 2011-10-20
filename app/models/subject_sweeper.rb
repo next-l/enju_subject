8d67fe28ade6a5d718bfff0dc588b11e73def1ff
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-20 17:17:28 +0900

    updated sweeper

--- app/models/subject_sweeper.rb
+++ app/models/subject_sweeper.rb
@@ -3,8 +3,8 @@ class SubjectSweeper < ActionController::Caching::Sweeper
   include ExpireEditableFragment
 
   def after_save(record)
-    case
-    when record.is_a?(Subject)
+    case record.to_s.to_sym
+    when :Subject
       expire_editable_fragment(record)
       record.works.each do |work|
         expire_editable_fragment(work)
@@ -12,14 +12,14 @@ class SubjectSweeper < ActionController::Caching::Sweeper
       record.classifications.each do |classification|
         expire_editable_fragment(classification)
       end
-    when record.is_a?(Classification)
+    when :Classification
       expire_editable_fragment(record)
       record.subjects.each do |subject|
         expire_editable_fragment(subject)
       end
-    when record.is_a?(SubjectHeadingTypeHasSubject)
+    when :SubjectHeadingTypeHasSubject
       expire_editable_fragment(record.subject)
-    when record.is_a?(WorkHasSubject)
+    when :WorkHasSubject
       expire_editable_fragment(record.work)
       expire_editable_fragment(record.subject)
     end
