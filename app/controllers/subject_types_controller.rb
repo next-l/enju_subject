afa5d40a2b9ebea8e7485c0686a96957a8c1f883
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-20 20:49:34 +0900

    removed check_client_ip_address

--- app/controllers/subject_types_controller.rb
+++ app/controllers/subject_types_controller.rb
@@ -1,6 +1,5 @@
 class SubjectTypesController < InheritedResources::Base
   respond_to :html, :xml
-  before_filter :check_client_ip_address
 
   def update
     @subject_type = SubjectType.find(params[:id])
