c9fa8b4bcf80522c46816fd8e4e3df4b2c92620a
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-11 04:35:15 +0900

    updated generator

--- lib/generators/enju_subject/setup/setup_generator.rb
+++ lib/generators/enju_subject/setup/setup_generator.rb
@@ -4,5 +4,7 @@ class EnjuSubject::SetupGenerator < Rails::Generators::Base
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_subject")
     rake("enju_subject_engine:install:migrations")
+    inject_into_file 'app/controllers/application_controller.rb',
+      "  enju_subject\n", :after => "enju_library\n"
   end
 end
