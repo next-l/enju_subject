dbc28f6d26dc256bdc3873f4daf9461c1262af11
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-05-05 21:10:11 +0900

    validate subject_heading_type_id

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -8,7 +8,7 @@ class Subject < ActiveRecord::Base
   belongs_to :required_role, :class_name => 'Role', :foreign_key => 'required_role_id'
 
   validates_associated :subject_type, :subject_heading_type
-  validates_presence_of :term, :subject_type_id #, :subject_heading_type_id
+  validates_presence_of :term, :subject_type_id, :subject_heading_type_id
 
   searchable do
     text :term
--- spec/factories/subject.rb
+++ spec/factories/subject.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :subject do |f|
     f.sequence(:term){|n| "subject_#{n}"}
+    f.subject_heading_type_id{FactoryGirl.create(:subject_heading_type).id}
     f.subject_type_id{FactoryGirl.create(:subject_type).id}
   end
 end
