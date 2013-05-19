53425e8822ac6c43456cb0c94379e4e41af9363f
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-05-19 14:51:14 +0900

    updated annotations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -25,20 +25,21 @@ end
 #
 # Table name: subjects
 #
-#  id                 :integer          not null, primary key
-#  parent_id          :integer
-#  use_term_id        :integer
-#  term               :string(255)
-#  term_transcription :text
-#  subject_type_id    :integer          not null
-#  scope_note         :text
-#  note               :text
-#  required_role_id   :integer          default(1), not null
-#  lock_version       :integer          default(0), not null
-#  created_at         :datetime
-#  updated_at         :datetime
-#  deleted_at         :datetime
-#  url                :string(255)
-#  manifestation_id   :integer
+#  id                      :integer          not null, primary key
+#  parent_id               :integer
+#  use_term_id             :integer
+#  term                    :string(255)
+#  term_transcription      :text
+#  subject_type_id         :integer          not null
+#  scope_note              :text
+#  note                    :text
+#  required_role_id        :integer          default(1), not null
+#  lock_version            :integer          default(0), not null
+#  created_at              :datetime
+#  updated_at              :datetime
+#  deleted_at              :datetime
+#  url                     :string(255)
+#  manifestation_id        :integer
+#  subject_heading_type_id :integer
 #
 
