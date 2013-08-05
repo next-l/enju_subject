533cfa515b27dab036ae3589b4630179f151ea37
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-08-05 18:36:06 +0900

    updated Gemfile

--- lib/enju_subject/engine.rb
+++ lib/enju_subject/engine.rb
@@ -1,6 +1,8 @@
 require 'enju_core'
 require 'inherited_resources'
 require 'dynamic_form'
+require 'protected_attributes'
+require 'rails-observers'
 
 module EnjuSubject
   class Engine < ::Rails::Engine
