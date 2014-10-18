462418099eda8eb2ae0d9a91280fd4edc0bca949
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-19 02:31:50 +0900

    updated dummy migration files

--- enju_subject.gemspec
+++ enju_subject.gemspec
@@ -20,6 +20,8 @@ Gem::Specification.new do |s|
   s.add_dependency "dynamic_form"
 
   s.add_development_dependency "sqlite3"
+  s.add_development_dependency "mysql2"
+  s.add_development_dependency "pg"
   s.add_development_dependency "rspec-rails", "~> 3.1"
   s.add_development_dependency "simplecov"
   s.add_development_dependency "factory_girl_rails"
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813182425) do
+ActiveRecord::Schema.define(version: 20140823095740) do
 
   create_table "accepts", force: true do |t|
     t.integer  "basket_id"
@@ -510,7 +510,6 @@ ActiveRecord::Schema.define(version: 20140813182425) do
     t.string   "access_address"
     t.integer  "language_id",                     default: 1,     null: false
     t.integer  "carrier_type_id",                 default: 1,     null: false
-    t.integer  "extent_id",                       default: 1,     null: false
     t.integer  "start_page"
     t.integer  "end_page"
     t.integer  "height"
@@ -552,9 +551,11 @@ ActiveRecord::Schema.define(version: 20140813182425) do
     t.integer  "month_of_publication"
     t.boolean  "fulltext_content"
     t.string   "doi"
-    t.boolean  "periodical"
+    t.boolean  "serial"
     t.text     "statement_of_responsibility"
     t.text     "publication_place"
+    t.text     "extent"
+    t.text     "dimensions"
   end
 
   add_index "manifestations", ["access_address"], name: "index_manifestations_on_access_address"
@@ -745,6 +746,7 @@ ActiveRecord::Schema.define(version: 20140813182425) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "error_message"
   end
 
   add_index "resource_import_results", ["item_id"], name: "index_resource_import_results_on_item_id"
