1cd2ec54d6d2cbc0665f1156e764855a9f3306aa
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-14 22:14:46 +0900

    fix spec files

--- spec/lib/generators/enju_subject/setup/setup_generator_spec.rb
+++ spec/lib/generators/enju_subject/setup/setup_generator_spec.rb
@@ -7,8 +7,8 @@ describe EnjuSubject::SetupGenerator, type: :generator do
   destination File.expand_path("tmp", Rails.root)
   before(:each) do
     prepare_destination
-    %w( app/controllers/application_controller.rb config/initializers/enju_leaf.rb ).each do |f|
-      dist_file = "#{Rails.root}/#{f}"
+    %w( app/controllers/application_controller.rb app/models/user.rb ).each do |f|
+      dist_file = "#{Rails.root}/tmp/#{f}"
       dist_dir = File.dirname(dist_file)
       mkdir_p dist_dir
       touch dist_file
