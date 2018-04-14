59a319d714b58e1b05c22076081faad0d7ba4649
Kosuke Tanabe <nabeta@fastmail.fm>
2018-04-14 19:58:28 +0900

    update README

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116152012) do
+ActiveRecord::Schema.define(version: 2017_01_16_152012) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
   create_table "accepts", id: :serial, force: :cascade do |t|
     t.integer "basket_id"
