eca55a14a19bb5887521b366a7c9686f6e7a7210
Kosuke Tanabe <nabeta@fastmail.fm>
2018-07-03 00:48:45 +0900

    add version to migration files

--- db/migrate/029_create_subjects.rb
+++ db/migrate/029_create_subjects.rb
@@ -1,4 +1,4 @@
-class CreateSubjects < ActiveRecord::Migration
+class CreateSubjects < ActiveRecord::Migration[4.2]
   def self.up
     create_table :subjects do |t|
       t.references :parent, index: true
