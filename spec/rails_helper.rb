165097cbfa3ae545df82f60856a0110c43bfce41
Kosuke Tanabe <nabeta@fastmail.fm>
2018-07-10 00:43:04 +0900

    disable sunspot-rails-tester

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,18 +34,6 @@ RSpec.configure do |config|
 
   config.extend ControllerMacros, type: :controller
 
-  $original_sunspot_session = Sunspot.session
-
-  config.before do
-    Sunspot.session = Sunspot::Rails::StubSessionProxy.new($original_sunspot_session)
-  end
-
-  config.before :each, solr: true do
-    Sunspot::Rails::Tester.start_original_sunspot_session
-    Sunspot.session = $original_sunspot_session
-    Sunspot.remove_all!
-  end
-
   config.infer_spec_type_from_file_location!
 end
 
