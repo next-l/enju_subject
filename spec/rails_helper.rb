15070aa0ad11dffed0a994cd51cd69e6a21b2249
Kosuke Tanabe <nabeta@fastmail.fm>
2018-07-10 00:44:18 +0900

    update spec file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_bot'
-require 'sunspot-rails-tester'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
