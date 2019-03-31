1011f2baf8e92eb247f8ff1fae1b8f30b28f66df
Kosuke Tanabe <nabeta@fastmail.fm>
2019-03-31 20:26:53 +0900

    update database.yml

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814165332) do
+ActiveRecord::Schema.define(version: 2016_08_14_165332) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer "basket_id"
