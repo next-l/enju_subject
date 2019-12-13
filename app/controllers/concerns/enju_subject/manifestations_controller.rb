9b06b2ff8f90947d2e3c1bc7d5ed45290016449f
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-13 23:58:20 +0900

    fix loading module

--- app/controllers/concerns/enju_subject/manifestations_controller.rb
+++ app/controllers/concerns/enju_subject/manifestations_controller.rb
@@ -4,21 +4,20 @@ module EnjuSubject
 
     included do
       before_action :get_subject, except: [:create, :update, :destroy]
+    end
 
-      private
-
-      def prepare_options
-        @carrier_types = CarrierType.select([:id, :display_name_translations, :position])
-        @content_types = ContentType.select([:id, :display_name_translations, :position])
-        @roles = Role.select([:id, :display_name_translations, :position])
-        @languages = Language.select([:id, :display_name, :position])
-        @frequencies = Frequency.select([:id, :display_name_translations, :position])
-        @identifier_types = IdentifierType.select([:id, :display_name, :position])
-        @nii_types = NiiType.select([:id, :display_name, :position]) if defined?(EnjuNii)
-        @subject_types = SubjectType.select([:id, :display_name, :position])
-        @subject_heading_types = SubjectHeadingType.select([:id, :display_name, :position])
-        @classification_types = ClassificationType.select([:id, :display_name, :position])
-      end
+    private
+    def prepare_options
+      @carrier_types = CarrierType.select([:id, :display_name_translations, :position])
+      @content_types = ContentType.select([:id, :display_name_translations, :position])
+      @roles = Role.select([:id, :display_name_translations, :position])
+      @languages = Language.select([:id, :display_name, :position])
+      @frequencies = Frequency.select([:id, :display_name_translations, :position])
+      @identifier_types = IdentifierType.select([:id, :display_name, :position])
+      @nii_types = NiiType.select([:id, :display_name, :position]) if defined?(EnjuNii)
+      @subject_types = SubjectType.select([:id, :display_name, :position])
+      @subject_heading_types = SubjectHeadingType.select([:id, :display_name, :position])
+      @classification_types = ClassificationType.select([:id, :display_name, :position])
     end
   end
 end
