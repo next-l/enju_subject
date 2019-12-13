2f7100ee94091b51ec9847e960a9c80237e186aa
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-13 21:59:46 +0900

    update loading class

--- app/controllers/manifestations_controller.rb
+++ app/controllers/manifestations_controller.rb
@@ -1,5 +1,3 @@
-require_dependency EnjuBiblio::Engine.config.root.join('app', 'controllers', 'manifestations_controller.rb').to_s
-
 class ManifestationsController < ApplicationController
   include EnjuSubject::ManifestationsController
 end
