9f6953d3a19f5bda10dad96d38ac605a5776adc3
Kosuke Tanabe <nabeta@fastmail.fm>
2020-04-27 21:36:39 +0900

    update prepare_option

--- app/controllers/concerns/enju_subject/manifestations_controller.rb
+++ app/controllers/concerns/enju_subject/manifestations_controller.rb
@@ -8,16 +8,10 @@ module EnjuSubject
 
     private
     def prepare_options
-      @carrier_types = CarrierType.select([:id, :display_name_translations, :position])
-      @content_types = ContentType.select([:id, :display_name_translations, :position])
-      @roles = Role.select([:id, :display_name_translations, :position])
-      @languages = Language.select([:id, :display_name, :position])
-      @frequencies = Frequency.select([:id, :display_name_translations, :position])
-      @identifier_types = IdentifierType.select([:id, :display_name, :position])
-      @nii_types = NiiType.select([:id, :display_name, :position]) if defined?(EnjuNii)
       @subject_types = SubjectType.select([:id, :display_name, :position])
       @subject_heading_types = SubjectHeadingType.select([:id, :display_name, :position])
       @classification_types = ClassificationType.select([:id, :display_name, :position])
+      super
     end
   end
 end
